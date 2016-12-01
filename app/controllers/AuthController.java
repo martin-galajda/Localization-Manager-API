558e5e3ef47200b2a78b5b9550c98fe83ac4e12b
martin.galajda <galajda.mato@gmail.com>
2016-12-01 08:42:56 +0100

    Cleaning code

--- app/controllers/AuthController.java
+++ app/controllers/AuthController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import play.libs.Json;
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.*;
 import play.filters.csrf.*;
@@ -9,10 +8,7 @@ import play.filters.csrf.*;
 import javax.inject.Inject;
 
 import play.libs.ws.*;
-import scala.concurrent.ExecutionContextExecutor;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
@@ -24,16 +20,12 @@ import java.security.SecureRandom;
 public class AuthController extends Controller {
 
 	@Inject WSClient ws;
-	@Inject ExecutionContextExecutor exec;
 	@Inject HttpExecutionContext ec;
 
 	public Result google() {
 		SecureRandom randomGenerator = new SecureRandom();
 		int state = randomGenerator.nextInt();
-		System.out.println("google");
-		System.err.println("google");
 
-		String clientSecret = "3djQduYEEVXCJ9kdg4JGC0L2";
 		String clientId = "1091217744160-poc33mmkke85docb2miaqjtuk8e0ocvp.apps.googleusercontent.com";
 		String redirectUri = "https://glacial-hollows-97055.herokuapp.com/auth/google/handler";
 		String prompt = "consent";
@@ -43,15 +35,6 @@ public class AuthController extends Controller {
 
 		String googleUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 
-		final CompletableFuture<JsonNode> future = new CompletableFuture<>();
-
-		WSRequest req = ws.url(googleUrl)
-				.setQueryParameter("redirect_uri", redirectUri)
-				.setQueryParameter("client_id", clientId)
-				.setQueryParameter("prompt", prompt)
-				.setQueryParameter("response_type", responseType)
-				.setQueryParameter("scope", scope)
-				.setQueryParameter("access_type", accessType);
 
 		googleUrl += "?" +
 				"redirect_uri=" + redirectUri +
@@ -66,10 +49,6 @@ public class AuthController extends Controller {
 	}
 
 	public CompletionStage<Result> handleGoogle() {
-		System.out.println("handle google");
-		System.out.println(request());
-		System.err.println("handle google");
-		System.err.println(request());
 		String code = request().getQueryString("code");
 		String clientId = "1091217744160-poc33mmkke85docb2miaqjtuk8e0ocvp.apps.googleusercontent.com";
 		String clientSecret = "3djQduYEEVXCJ9kdg4JGC0L2";
@@ -88,32 +67,24 @@ public class AuthController extends Controller {
 		req.setContentType("application/x-www-form-urlencoded").post(reqForm).thenApplyAsync(response -> {
 			JsonNode jsonBody = response.asJson();
 			String accessToken = jsonBody.findPath("access_token").asText();
-			System.out.println(accessToken);
-			System.err.println("got access token");
-			System.err.println(jsonBody);
 			String refreshToken = jsonBody.findPath("refresh_token").asText();
 
 			WSRequest accessRequest = ws.url("https://www.googleapis.com/oauth2/v2/userinfo");
 			WSRequest authReq = accessRequest.setHeader("Authorization", "Bearer " + accessToken);
-			System.err.println(authReq);
 
 			CompletionStage<WSResponse> authFuture = authReq.get();
 
 
 			authFuture.thenApplyAsync(res -> {
 				JsonNode jsonBodyRes = res.asJson();
-				System.out.println(jsonBodyRes);
 				String name = jsonBodyRes.findPath("name").asText();
 				String id = jsonBodyRes.findPath("id").asText();
-				System.out.println(name);
-				System.out.println(id);
 				future.complete(jsonBodyRes);
 				return jsonBodyRes;
 			}, ec.current());
 			return jsonBody;
 		}, ec.current());
 
-		Http.Session sessionData = session();
 		return future.thenApplyAsync(res -> {
 			String name = res.findPath("name").asText();
 			String id = res.findPath("id").asText();
