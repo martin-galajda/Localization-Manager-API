583b1b577fde7b9850386e0e1613c94e86987fa7
martin.galajda <galajda.mato@gmail.com>
2016-12-09 00:06:58 +0100

    Fix

--- app/Filters.java
+++ app/Filters.java
@@ -29,8 +29,8 @@ public class Filters extends DefaultHttpFilters {
     /**
      */
     @Inject
-    public Filters(CSRFFilter csrfFilter, ExampleFilter exampleFilter) {
-        super(csrfFilter, exampleFilter);
+    public Filters(CSRFFilter csrfFilter) {
+        super(csrfFilter);
     }
 
 
