d7c0fdf03415205496966819743da6335c53b103
martin.galajda <galajda.mato@gmail.com>
2017-02-05 00:13:18 +0100

    FIx

--- app/controllers/AuthController.java
+++ app/controllers/AuthController.java
@@ -72,11 +72,13 @@ public class AuthController extends Controller {
 					user.setId(userReference.getKey());
 					user.setIdFromProvider(userProviderId);
 					userReference.setValue(user);
+					System.err.println("Not success user" + user);
 
 					future.complete(user);
 				} else {
 					User user = dataSnapshot.getValue(User.class);
 
+					System.err.println("Success user" + user);
 					future.complete(user);
 				}
 			}
@@ -93,6 +95,7 @@ public class AuthController extends Controller {
 	private CompletionStage<User> saveUserInfoInSession(CompletionStage<User> future)
 	{
 		return future.thenApplyAsync(user -> {
+
 			session().put(SESSION_USER_ID_FIELD, user.getId());
 			session().put(SESSION_USER_PROVIDER_ID_FIELD, user.getIdFromProvider());
 			session().put(SESSION_USER_NAME_FIELD, user.getName());
@@ -160,9 +163,9 @@ public class AuthController extends Controller {
 	}
 
 	private void setHeaders() {
-		//response().setHeader("Access-Control-Allow-Origin", "https://morning-taiga-56897.herokuapp.com");
-		//response().setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT");
-		//response().setHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With, X-XSRF-TOKEN");
-		//response().setHeader("Access-Control-Allow-Credentials", "true");
+		response().setHeader("Access-Control-Allow-Origin", "https://morning-taiga-56897.herokuapp.com");
+		response().setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT");
+		response().setHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With, X-XSRF-TOKEN");
+		response().setHeader("Access-Control-Allow-Credentials", "true");
 	}
 }
