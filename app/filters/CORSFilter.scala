f9b380178385fcd0db8b696912b78bc99afebfdf
martin.galajda <galajda.mato@gmail.com>
2017-02-04 23:58:24 +0100

    Fix

--- app/filters/CORSFilter.scala
+++ app/filters/CORSFilter.scala
@@ -1,8 +1,11 @@
 package filters
 
 import javax.inject.Inject
+
 import akka.stream.Materializer
+import play.api.http.Status
 import play.api.mvc._
+
 import scala.concurrent.{ExecutionContext, Future}
 
 class CORSFilter @Inject() (implicit val mat: Materializer, ec: ExecutionContext) extends Filter {
@@ -11,15 +14,18 @@ class CORSFilter @Inject() (implicit val mat: Materializer, ec: ExecutionContext
            (requestHeader: RequestHeader): Future[Result] = {
 
     println(requestHeader)
-    System.err.println(requestHeader)
     nextFilter(requestHeader).map { result =>
       System.err.println(result)
-      result.withHeaders(
-        "Access-Control-Allow-Origin" -> "https://morning-taiga-56897.herokuapp.com",
-        "Access-Control-Allow-Methods" -> "GET, POST, OPTIONS, DELETE, PUT",
-        "Access-Control-Allow-Headers" -> "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With, X-XSRF-TOKEN",
-        "Access-Control-Allow-Credentials" -> "true"
-      )
+      if (result.header.status != Status.SEE_OTHER) {
+        result.withHeaders(
+          "Access-Control-Allow-Origin" -> "https://morning-taiga-56897.herokuapp.com",
+          "Access-Control-Allow-Methods" -> "GET, POST, OPTIONS, DELETE, PUT",
+          "Access-Control-Allow-Headers" -> "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With, X-XSRF-TOKEN",
+          "Access-Control-Allow-Credentials" -> "true"
+        )
+      } else {
+        result
+      }
     }
   }
 }
