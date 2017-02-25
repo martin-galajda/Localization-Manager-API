1421f43f16079be2d22e4e66fc4ef5de6000a9d2
martin.galajda <galajda.mato@gmail.com>
2017-02-25 13:19:27 +0100

    Fix

--- app/controllers/SecuredController.java
+++ app/controllers/SecuredController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import play.Logger;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Security;
@@ -12,6 +13,8 @@ public class SecuredController extends Security.Authenticator {
 
 	@Override
 	public String getUsername(Http.Context ctx) {
+		Logger.debug("Inside secured controller");
+		Logger.debug(String.format("%s", ctx.session()));
 		return ctx.session().get(AuthService.SESSION_USER_NAME_FIELD);
 	}
 
