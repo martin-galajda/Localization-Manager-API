256e1db25a6d29694041fdc1a67b0716f2e56dfe
martin.galajda <galajda.mato@gmail.com>
2017-02-25 12:52:58 +0100

    fix

--- app/filters/CORSFilter.scala
+++ app/filters/CORSFilter.scala
@@ -20,7 +20,7 @@ class CORSFilter @Inject() (implicit val mat: Materializer, ec: ExecutionContext
         //"Access-Control-Allow-Origin" -> "http://localhost:3000",
         "Access-Control-Allow-Origin" -> "https://morning-taiga-56897.herokuapp.com",
         "Access-Control-Allow-Methods" -> "GET, POST, OPTIONS, DELETE, PUT",
-        "Access-Control-Allow-Headers" -> "Origin, X-Json, X-Prototype-Version, X-Requested-With, X-XSRF-TOKEN",
+        "Access-Control-Allow-Headers" -> "Accept, Origin, Content-type, X-Json, X-Prototype-Version, X-Requested-With, X-XSRF-TOKEN",
         "Access-Control-Allow-Credentials" -> "true"
       )
     }
