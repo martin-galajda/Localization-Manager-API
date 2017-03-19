2770839be00b2ee544b336f258b76c6d45582f28
martin.galajda <galajda.mato@gmail.com>
2017-03-19 08:33:20 +0100

    Fix error handling

--- ErrorHandler.java
+++ ErrorHandler.java
@@ -8,7 +8,7 @@ import javax.inject.Singleton;
 @Singleton
 public class ErrorHandler implements HttpErrorHandler {
 	public CompletionStage<Result> onClientError(RequestHeader request, int statusCode, String message) {
-		System.err.println("message" + message);
+		System.err.println("message " + message);
 
 		return CompletableFuture.completedFuture(
 				Results.status(statusCode, "A client error occurred: " + message)
@@ -16,7 +16,7 @@ public class ErrorHandler implements HttpErrorHandler {
 	}
 
 	public CompletionStage<Result> onServerError(RequestHeader request, Throwable exception) {
-		System.err.println("message" + exception.getMessage());
+		System.err.println("message " + exception.getMessage());
 
 		return CompletableFuture.completedFuture(
 				Results.internalServerError("A server error occurred: " + exception.getMessage())
